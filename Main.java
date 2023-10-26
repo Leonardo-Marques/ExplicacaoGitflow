b345770349fc2871167ef154be7a64f67393bcaf
Leonardo marques <32113853+Leonardo-Marques@users.noreply.github.com>
2023-10-26 16:55:33 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println("feature 2");
 
         System.out.println("AQUI vou codar os cenarios da branch feature sacola");
-        //merge
     }
 
     //alteracao2
